5285fd1da66e35728c7984e8515a46eedc87f5a4
Kateryna Morhun <kmorhun@mit.edu>
2024-02-22 15:41:23 -0500

    moved current page highlight to js

--- global.js
+++ global.js
@@ -3,3 +3,9 @@ console.log("IT’S ALIVE!");
 function $$ (selector, context = document) {
 	return Array.from(context.querySelectorAll(selector));
 }
+
+let navLinks = $$("nav a")
+let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname)
+
+// add css styling class, as long as currentLink is not undefined
+currentLink?.classList.add("current-page")
