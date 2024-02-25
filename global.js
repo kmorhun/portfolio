3b04aa8ba41516e599fc3a0cc5dc752dbd5d4e83
Kateryna Morhun <kmorhun@mit.edu>
2024-02-25 16:17:10 -0500

    got github link working locally

--- global.js
+++ global.js
@@ -15,20 +15,33 @@ let pages = {
     "projects/": "Projects",
     "cv/": "CV",
     "contact/": "Contact",
-    "github.com/kmorhun": "Follow Me on Github~" ,
+    "https://github.com/kmorhun": "Follow Me on Github~" ,
 }
 
 const ARE_WE_HOME = document.documentElement.classList.contains("home");
-console.log("ARE_WE_HOME", ARE_WE_HOME);
+// console.log("ARE_WE_HOME", ARE_WE_HOME);
 let nav = document.createElement("nav");
 document.body.prepend(nav);
 
 for (let url in pages) {
     let title = pages[url];
-    url = (ARE_WE_HOME && !url.startsWith("https") && !url.startsWith("http")) ? url : "../" + url;
+    console.log("RAW URL", url);
+
+    // make the URL relative if you're not on the home page, or if deployed
+    url = (ARE_WE_HOME || url.startsWith("https")) ? url : "../" + url;
+    console.log("MODIFIED URL", url);
     let a = document.createElement("a");
     a.href = url;
     a.textContent = title;
+    console.log("a.host", a.host, "location.host", location.host, "a.pathname", a.pathname, "location.pathname", location.pathname)
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }
+    if (a.host === location.host && a.pathname ===location.pathname) {
+        a.classList.add("current-page");
+    }
     nav.append(a);
     // nav.insertAdjacentHTML("beforeend", `<a href="${url}">${title}</a>`);
+
+
 }
