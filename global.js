189c87db5690e2cb71fa774c007f186d77323cd2
Kateryna Morhun <kmorhun@mit.edu>
2024-02-25 23:14:42 -0500

    intercepted contact form with js

--- global.js
+++ global.js
@@ -70,3 +70,17 @@ select.addEventListener("input", function (event) {
     document.documentElement.style.setProperty("color-scheme", event.target.value);
     localStorage.colorScheme = event.target.value;
 });
+
+// intercept the contact form
+let contact_form = document.querySelector("form");
+contact_form?.addEventListener("submit", function (event) {
+    // prevent the default submission
+    event.preventDefault();
+    let data = new FormData(contact_form);
+    let url = contact_form.action + "?"
+    for (let [name, value] of data) {
+        url += `${name}=${encodeURIComponent(value)}&`;
+        // console.log(name, encodeURIComponent(value));
+    }
+    location.href = url;
+});
